d39141acae6f0bccd53f912fd752ba5ad057ad13
skorpion <savinov_vu@inbox.ru>
2017-04-20 13:33:05 +0800

    button action.

--- src/ru/inbox/savinov_vu/Controller.java
+++ src/ru/inbox/savinov_vu/Controller.java
@@ -1,4 +1,17 @@
 package ru.inbox.savinov_vu;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+
+    public void firstButtonAction(ActionEvent actionEvent) {
+        System.out.println("button1");
+    }
+
+    public void secondButtonAction(ActionEvent actionEvent) {
+        System.out.println("button2");
+
+    }
+
+
 }
