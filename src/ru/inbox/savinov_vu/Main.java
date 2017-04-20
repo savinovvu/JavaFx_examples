823d1c16c5b944aadc0f27e9c82b49db3467bfb5
skorpion <savinov_vu@inbox.ru>
2017-04-20 12:13:32 +0800

    button with center form with java fxml.

--- src/ru/inbox/savinov_vu/Main.java
+++ src/ru/inbox/savinov_vu/Main.java
@@ -1,11 +1,9 @@
 package ru.inbox.savinov_vu;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -15,14 +13,9 @@ public class Main extends Application {
     }
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Button button = new Button("test");
-        Text text = new Text(10, 20, " testfx");
-        text.setFont(new Font(40));
-        BorderPane pane = new BorderPane();
-        pane.setCenter(button);
-        pane.setTop(text);
-        Scene scene = new Scene(pane, 400, 400);
-        primaryStage.setTitle("title");
+        Parent panel = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene = new Scene(panel, 400, 400);
+        primaryStage.setTitle("Welcome");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
