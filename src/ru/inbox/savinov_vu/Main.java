126cd9126363f9150da71620862506096bfb9fc0
skorpion <savinov_vu@inbox.ru>
2017-04-20 12:00:36 +0800

    button with center form with java Code.

--- src/ru/inbox/savinov_vu/Main.java
+++ src/ru/inbox/savinov_vu/Main.java
@@ -1,6 +1,11 @@
 package ru.inbox.savinov_vu;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,6 +15,15 @@ public class Main extends Application {
     }
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Button button = new Button("test");
+        Text text = new Text(10, 20, " testfx");
+        text.setFont(new Font(40));
+        BorderPane pane = new BorderPane();
+        pane.setCenter(button);
+        pane.setTop(text);
+        Scene scene = new Scene(pane, 400, 400);
+        primaryStage.setTitle("title");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
